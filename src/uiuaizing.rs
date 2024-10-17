0db257c454beba8f51f17d01620e0c6636abe475
amatgil <amatgilvinyes@gmail.com>
2024-10-17 20:48:38 +0200

    Prettify examples

--- src/uiuaizing.rs
+++ src/uiuaizing.rs
@@ -6,17 +6,23 @@ use uiua::format::*;
 use uiua::{PrimDocFragment, PrimDocLine, Primitive, Uiua};
 
 use base64::engine::general_purpose::URL_SAFE;
-use std::sync::LazyLock;
 use std::collections::HashMap;
+use std::sync::LazyLock;
 
 const DEFAULT_EXECUTION_LIMIT: Duration = Duration::from_secs(2);
 const EMOJI_IDS: &'static str = include_str!("../assets/glyphlist.txt");
 static EMOJI_MAP: LazyLock<HashMap<&str, &str>> = LazyLock::new(|| {
-    EMOJI_IDS.lines().map(|l| {
-        let space_idx = l.bytes().position(|c| c == b' ').expect("EMOJI_IDS are malformed");
-        let (a, b) = l.split_at(space_idx);
-        (a, &b[1..])
-    }).collect::<HashMap<&str, &str>>()
+    EMOJI_IDS
+        .lines()
+        .map(|l| {
+            let space_idx = l
+                .bytes()
+                .position(|c| c == b' ')
+                .expect("EMOJI_IDS are malformed");
+            let (a, b) = l.split_at(space_idx);
+            (a, &b[1..])
+        })
+        .collect::<HashMap<&str, &str>>()
 });
 
 pub fn run_uiua(code: &str) -> String {
@@ -54,7 +60,16 @@ pub fn get_docs(f: &str) -> String {
         .or_else(|| Primitive::from_name(f))
     {
         Some(docs) => {
-            let d = docs
+            let short = docs
+                .doc()
+                .short
+                .iter()
+                .map(print_doc_frag)
+                .map(|s| format!("## {s}"))
+                .collect::<Vec<String>>()
+                .join("\n");
+
+            let long = docs
                 .doc()
                 .lines
                 .iter()
@@ -62,9 +77,26 @@ pub fn get_docs(f: &str) -> String {
                 .map(print_docs)
                 .collect::<Vec<String>>()
                 .join("\n");
-            format!("Documentation ([link](https://uiua.org/docs/{f})):\n{d}")
+            format!("Documentation ([link](https://uiua.org/docs/{f})):\n{short}\n\n\n{long}")
         }
         None => format!("No docs found for '{f}', did you spell it right?"),
+    }
+}
+
+fn print_doc_frag(frag: &PrimDocFragment) -> String {
+    match frag {
+        PrimDocFragment::Text(t) => t.clone(),
+        PrimDocFragment::Code(t) => format!("`{t}`"),
+        PrimDocFragment::Emphasis(t) => format!("_{t}_"),
+        PrimDocFragment::Strong(t) => format!("**{t}**"),
+        PrimDocFragment::Primitive { prim, named } => {
+            if *named {
+                format!("{} `{}`", print_emoji(&prim), prim.name())
+            } else {
+                print_emoji(&prim)
+            }
+        }
+        PrimDocFragment::Link { text, url } => format!("[{text}]({url})"),
     }
 }
 
@@ -72,35 +104,17 @@ fn print_docs(line: &PrimDocLine) -> String {
     match line {
         PrimDocLine::Text(vs) => vs
             .into_iter()
-            .map(|v| match v {
-                PrimDocFragment::Text(t) => t.clone(),
-                PrimDocFragment::Code(t) => format!("`{t}`"),
-                PrimDocFragment::Emphasis(t) => format!("_{t}_"),
-                PrimDocFragment::Strong(t) => format!("**{t}**"),
-                PrimDocFragment::Primitive { prim, named } => {
-                    if *named {
-                        format!("{} `{}`", print_emoji(prim), prim.name())
-                    } else {
-                        print_emoji(prim)
-                    }
-                }
-                PrimDocFragment::Link { text, url } => format!("[{text}]({url})"),
-            })
+            .map(print_doc_frag)
             .collect::<Vec<String>>()
             .join(" "),
         PrimDocLine::Example(e) => {
-            format!(
-                "```
-{}
-# {}
-```
-",
-                e.input(),
-                match e.output().as_ref().map(|vs| vs.join(";")) {
-                    Ok(l) => format!("> {l}"),
-                    Err(l) => format!("> {l}"),
-                }
-            )
+            let out = match e.output().as_ref().map(|vs| vs.join(";")) {
+                Ok(l) => l,
+                Err(l) => l.to_string(),
+            };
+            let text = format!("{}\n# {}", e.input(), out);
+
+            format!("{}",highlight_code(&text))
         }
     }
 }
