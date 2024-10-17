73c48bbdfc0c928907affe9c494c332032fdb39b
Abram Bradley <abramabradley@gmail.com>
2024-10-17 15:18:58 -0400

    Updated links to handle www

--- src/handle_raw_pad_links.rs
+++ src/handle_raw_pad_links.rs
@@ -2,7 +2,7 @@ use regex::Regex;
 use std::vec::Vec;
 
 const MARKDOWN_LINK_RE: &str = r"\[.*?\]\(.*?\)";
-const UIUA_PAD_LINK_RE: &str = r"https?://www.uiua.org/pad\?src=[0-9a-zA-Z_\-=]+";
+const UIUA_PAD_LINK_RE: &str = r"https?://(www\.)?uiua\.org/pad\?src=[0-9a-zA-Z_\-=]+";
 
 fn strip_markdown_links(message: &str) -> String {
     Regex::new(MARKDOWN_LINK_RE)
@@ -29,7 +29,10 @@ pub fn has_raw_pad_link(message: &str) -> bool {
 mod tests {
     use super::*;
 
-    const RAW_PAD_LINK: &str = "https://www.uiua.org/pad?src=0_13_0-rc_4__4o2c4oqaCg==";
+    const HTTP_WWW_RAW_PAD_LINK: &str = "http://www.uiua.org/pad?src=0_13_0-rc_4__4o2c4oqaCg==";
+    const HTTPS_WWW_RAW_PAD_LINK: &str = "https://www.uiua.org/pad?src=0_13_0-rc_4__4o2c4oqaCg==";
+    const HTTP_RAW_PAD_LINK: &str = "http://uiua.org/pad?src=0_13_0-rc_4__4o2c4oqaCg==";
+    const HTTPS_RAW_PAD_LINK: &str = "https://uiua.org/pad?src=0_13_0-rc_4__4o2c4oqaCg==";
     const MD_PAD_LINK: &str = "[uiua](https://www.uiua.org/pad?src=0_13_0-rc_4__4o2c4oqaCg==)";
     const MD_LINK: &str = "[google](https://google.com)";
 
@@ -37,15 +40,32 @@ mod tests {
     fn base_link_allowed() {
         assert!(extract_raw_pad_link("http://www.uiua.org/pad").is_empty());
         assert!(extract_raw_pad_link("https://www.uiua.org/pad").is_empty());
+        assert!(extract_raw_pad_link("http://uiua.org/pad").is_empty());
+        assert!(extract_raw_pad_link("https://uiua.org/pad").is_empty());
     }
 
     #[test]
     fn raw_link_disallowed() {
         assert_eq!(
-            extract_raw_pad_link("http://www.uiua.org/pad?src=0_13_0-rc_4__4o2c4oqaCg=="),
-            vec!["http://www.uiua.org/pad?src=0_13_0-rc_4__4o2c4oqaCg=="],
+            extract_raw_pad_link(HTTP_WWW_RAW_PAD_LINK),
+            vec![HTTP_WWW_RAW_PAD_LINK],
         );
-        assert_eq!(extract_raw_pad_link(RAW_PAD_LINK), vec![RAW_PAD_LINK]);
+        assert_eq!(
+            extract_raw_pad_link(HTTPS_WWW_RAW_PAD_LINK),
+            vec![HTTPS_WWW_RAW_PAD_LINK],
+        );
+        assert_eq!(
+            extract_raw_pad_link(HTTP_RAW_PAD_LINK),
+            vec![HTTP_RAW_PAD_LINK],
+        );
+        assert_eq!(
+            extract_raw_pad_link(HTTPS_RAW_PAD_LINK),
+            vec![HTTPS_RAW_PAD_LINK],
+        );
+        assert_eq!(
+            extract_raw_pad_link(format!("{HTTP_WWW_RAW_PAD_LINK} {HTTPS_WWW_RAW_PAD_LINK} {HTTP_RAW_PAD_LINK} {HTTPS_RAW_PAD_LINK}").as_str()),
+            vec![HTTP_WWW_RAW_PAD_LINK, HTTPS_WWW_RAW_PAD_LINK, HTTP_RAW_PAD_LINK, HTTPS_RAW_PAD_LINK],
+        );
     }
 
     #[test]
@@ -77,29 +97,38 @@ mod tests {
     #[test]
     fn both() {
         assert_eq!(
-            extract_raw_pad_link(format!("{RAW_PAD_LINK} and some stuff {MD_PAD_LINK}").as_str()),
-            vec![RAW_PAD_LINK],
+            extract_raw_pad_link(
+                format!("{HTTPS_WWW_RAW_PAD_LINK} and some stuff {MD_PAD_LINK}").as_str()
+            ),
+            vec![HTTPS_WWW_RAW_PAD_LINK],
         );
         assert_eq!(
             extract_raw_pad_link(
-                format!("{RAW_PAD_LINK} and some stuff {MD_PAD_LINK} {RAW_PAD_LINK} and some more stuff {MD_PAD_LINK}")
+                format!("{HTTPS_WWW_RAW_PAD_LINK} and some stuff {MD_PAD_LINK} {HTTPS_WWW_RAW_PAD_LINK} and some more stuff {MD_PAD_LINK}")
                     .as_str()
             ),
-            vec![RAW_PAD_LINK, RAW_PAD_LINK]
+            vec![HTTPS_WWW_RAW_PAD_LINK, HTTPS_WWW_RAW_PAD_LINK]
         );
     }
 
     #[test]
     fn strips_with_both() {
         assert_eq!(
-            strip_markdown_links(format!("{RAW_PAD_LINK} and some stuff {MD_PAD_LINK}").as_str()),
-            format!("{RAW_PAD_LINK} and some stuff "),
+            strip_markdown_links(
+                format!("{HTTPS_WWW_RAW_PAD_LINK} and some stuff {MD_PAD_LINK}").as_str()
+            ),
+            format!("{HTTPS_WWW_RAW_PAD_LINK} and some stuff "),
         );
         assert_eq!(strip_markdown_links(
-                format!("{RAW_PAD_LINK} and some stuff {MD_PAD_LINK} {RAW_PAD_LINK} and some more stuff {MD_PAD_LINK}")
+                format!("{HTTPS_WWW_RAW_PAD_LINK} and some stuff {MD_PAD_LINK} {HTTPS_WWW_RAW_PAD_LINK} and some more stuff {MD_PAD_LINK}")
                     .as_str()
             ),
-            format!("{RAW_PAD_LINK} and some stuff  {RAW_PAD_LINK} and some more stuff ")
+            format!("{HTTPS_WWW_RAW_PAD_LINK} and some stuff  {HTTPS_WWW_RAW_PAD_LINK} and some more stuff ")
+        );
+        // "https://www.uiua.org/pad?src=0_13_0-rc_4__4o2c4oqaCg=="
+        assert_eq!(
+            extract_raw_pad_link("https://uiua.org/pad?src=0_13_0-rc_4__4o2c4oqaCg=="),
+            vec!["https://uiua.org/pad?src=0_13_0-rc_4__4o2c4oqaCg=="]
         );
     }
 }
