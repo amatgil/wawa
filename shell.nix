219f52089131c8629050d2626d6129f7a7d16faf
amatgil <amatgilvinyes@gmail.com>
2024-11-24 00:42:03 +0100

    Use nightly

--- shell.nix
+++ shell.nix
@@ -6,7 +6,8 @@
 pkgs.mkShell {
   # Pinned packages available in the environment
   packages = with pkgs; [
-    rust-bin.stable.latest.default
+    #rust-bin.stable.latest.default
+    (rust-bin.selectLatestNightlyWith (toolchain: toolchain.default))
     cargo-bloat
     rust-analyzer
     pkg-config
