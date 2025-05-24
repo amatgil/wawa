b30297cfcf6c60c35ceba7b2af077ce3ce35baf6
amatgil <amatgilvinyes@gmail.com>
2025-05-24 12:14:44 +0200

    wawa now replies if it's killed while it's handling a response

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-pub use std::sync::Arc;
-use std::sync::LazyLock;
-
+use serde::{Deserialize, Serialize};
 use serenity::{
     all::{Reaction, ReactionType, Ready},
     async_trait,
     model::channel::Message,
     prelude::*,
+};
+pub use std::sync::Arc;
+use std::{
+    fs::{self, read_to_string, File},
+    io::{Read, Write},
+    path::PathBuf,
+    sync::LazyLock,
 };
 use tracing::{debug, error, info, instrument, span, trace, Level};
 use tracing_appender::rolling::{RollingFileAppender, Rotation};
@@ -14,6 +19,8 @@ use tracing_subscriber::{
     EnvFilter,
 };
 use wawa::*;
+
+const ANSWERED_MESSAGES_PATH: &str = "/tmp/wawa_answered_messages_path/";
 
 static SELF_HANDLE: LazyLock<String> =
     LazyLock::new(|| dotenv::var("BOT_SELF_HANDLE").unwrap_or_else(|_| "wawa#0280".into()));
@@ -71,6 +78,20 @@ async fn handle_message(ctx: Context, msg: Message) {
         let _guard = span.enter();
         info!(user = msg.author.name, body = ?s, "Processing body");
 
+        info!("saving as pending to answeredness file");
+        let answered_path = PathBuf::from(ANSWERED_MESSAGES_PATH);
+        let mut random_hash: u128 = rand::random();
+        while let Ok(true) = fs::exists(answered_path.join(random_hash.to_string())) {
+            random_hash = rand::random();
+        }
+        let answered_lock_file_path = answered_path.join(random_hash.to_string());
+        match fs::File::create(&answered_lock_file_path) {
+            Err(e) => error!(?e, "could not create file in answeredness direcory"),
+            Ok(mut f) => {
+                _ = f.write(serenity::json::to_string(&msg).unwrap().as_bytes());
+            }
+        };
+
         let s = s.trim();
         let space_idx = s
             .bytes()
@@ -90,6 +111,18 @@ async fn handle_message(ctx: Context, msg: Message) {
             "s" | "show" => handle_show(msg, ctx.http, s[space_idx..].trim()).await,
             "shutdown" => send_message(msg, &ctx.http, "Ok, shutting down now").await, // This does not shutdown
             unrec => handle_unrecognized(msg, ctx.http, unrec).await,
+        }
+
+        match fs::remove_file(&answered_lock_file_path) {
+            Ok(()) => info!(
+                "answering complete, deleting file '{}'",
+                answered_lock_file_path.display()
+            ),
+            Err(e) => error!(
+                ?e,
+                "failed to remove answeredness file at '{}'",
+                answered_lock_file_path.display()
+            ),
         }
     } else {
         let span = span!(Level::TRACE, "rulethree_handler");
@@ -124,12 +157,38 @@ impl EventHandler for Handler {
     async fn ready(&self, ctx: Context, ready: Ready) {
         info!(name = ready.user.name, "Bot is connected");
         trace!("deleting global commands, just in case");
-        dbg!(
-            serenity::model::application::Command::set_global_commands(ctx.http, vec![])
-                .await
-                .unwrap()
-        );
+        serenity::model::application::Command::set_global_commands(&ctx.http, vec![])
+            .await
+            .unwrap();
         trace!("deleted global commands");
+
+        // Respond to all messages that failed
+        info!("Responding to all messages that wawa started to respond to, but didn't finalize");
+
+        _ = fs::create_dir(ANSWERED_MESSAGES_PATH);
+        let paths = fs::read_dir(ANSWERED_MESSAGES_PATH).unwrap();
+        for path in paths {
+            // SAFETY: if something happens between opening the dir and reading the path,
+            // we want to panic anyway
+            let path = path.as_ref().unwrap().path();
+            match read_to_string(&path) {
+                Err(e) => error!(?e, "could not read from answered file"),
+                Ok(contents) => {
+                    trace!(path = ?path, "read file with ok");
+                    let msg: Message = match serde_json::from_str(&contents) {
+                        Ok(m) => m,
+                        Err(e) => {
+                            error!(?path, ?e, "could not read json message from path");
+                            continue;
+                        }
+                    };
+                    send_message(msg, &ctx.http,
+                                 "Wawa seems to have been shutdown during the computation you summoned! It's likely it triggered the Out-Of-Memory-Killer; please try to not send messages that use up that much memory").await;
+                    info!(?path, "responded with OOM to path");
+                    _ = fs::remove_file(path);
+                }
+            }
+        }
     }
     async fn reaction_add(&self, ctx: Context, reaction: Reaction) {
         let reacted_message = match reaction.message(&ctx.http).await {
